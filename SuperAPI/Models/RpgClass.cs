e2195ed6efd60b99151ebc34a46160c069d2289b
rrvillareal23 <rrvillareal@arizona.edu.com>
2024-02-16 19:23:26 -0800

    Added RpgClass for Character Class Types

---
+++ SuperAPI/Models/RpgClass.cs
@@ -0,0 +1,12 @@
+namespace SuperAPI.Models
+{
+    public enum RpgClass
+    {
+        Cosmic = 1,
+        Mutant = 2,
+        Mystic = 3,
+        Science = 4,
+        Skill = 5,
+        Tech = 6
+    }
+}
